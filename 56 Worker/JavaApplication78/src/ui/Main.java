2301520315c5f13ebd719aea846f9fa77ec109b0
TranHoan2709 <hoana5k44nknd@gmail.com>
2024-05-21 09:03:36 +0700

    LAB211_56

--- 56 Worker/JavaApplication78/src/ui/Main.java
+++ 56 Worker/JavaApplication78/src/ui/Main.java
@@ -4,13 +4,38 @@
  */
 package ui;
 
+import controller.ManagerController;
+import utils.ValidationAndNormalization;
+
 /**
  *
  * @author ADMIN
  */
 public class Main {
     public static void main(String[] args) {
-        Menu menu = new Menu();
-        menu.menu(); 
+        ManagerController managerController = new ManagerController();
+        String menu = """
+                      ======= Worker Managerment =======
+                          1. Add Worker
+                          2. Up salary
+                          3. Down salary
+                          4. Display Information salary
+                          5. Exit
+                      
+                      """;
+        
+        while (true) {
+            System.out.print(menu);
+            int choice = ValidationAndNormalization.checkInputInRange("Your choice: ", "Please enter an integer number from 1 to 5", 1, 5);
+            switch (choice) {
+                case 1 -> managerController.addWorker();
+                case 2 -> managerController.upSalary();
+                case 3 -> managerController.downSalary();
+                case 4 -> managerController.getInformation();
+                case 5 -> {
+                    return;
+                }
+            }
+        }
     }
 }
